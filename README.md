7a0b87b005aed6d418b788a1e965de3c0781a21f
X.C. Wong <sendme@qq.com>
2021-08-26 00:19:15 +0800

    feat(gateway predicate): 自定义 Gateway 断言

--- README.md
+++ README.md
@@ -14,11 +14,14 @@ sh startup.sh -m standalone
 ```
 
 ### 启动 sentinel dashboard
+
 ```shell
 cd sentinel
 java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard-1.8.2.jar
 ```
+
 默认账号密码
+
 ```shell
-账号：sentinel / 密码：sentinel
+账号: sentinel / 密码: sentinel
 ```
--- service-gateway/src/main/java/cn/chingshen/predicates/AgeRoutePredicateFactory.java
+++ service-gateway/src/main/java/cn/chingshen/predicates/AgeRoutePredicateFactory.java
@@ -1,8 +1,10 @@
 package cn.chingshen.predicates;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 
 import java.util.Arrays;
@@ -18,8 +20,8 @@ import java.util.function.Predicate;
  */
 @Component
 public class AgeRoutePredicateFactory extends AbstractRoutePredicateFactory<AgeRoutePredicateFactory.Config> {
-    public AgeRoutePredicateFactory(Class<Config> configClass) {
-        super(configClass);
+    public AgeRoutePredicateFactory() {
+        super(Config.class);
     }
 
     @Override
@@ -30,11 +32,25 @@ public class AgeRoutePredicateFactory extends AbstractRoutePredicateFactory<AgeR
 
     @Override
     public Predicate<ServerWebExchange> apply(Config config) {
-        return null;
+        return new Predicate<ServerWebExchange>() {
+            @Override
+            public boolean test(ServerWebExchange serverWebExchange) {
+                // 接受前台传入的 age 参数
+                // 是否能为空
+                // 如果不为空，则进行路由逻辑判断
+                String ageStr = serverWebExchange.getRequest().getQueryParams().getFirst("age");
+                if (!StringUtils.isEmpty(ageStr)) {
+                    int age = Integer.parseInt(ageStr);
+                    return age >= config.min && age <= config.max;
+                }
+                return false;
+            }
+        };
     }
 
 
     @Data
+    @NoArgsConstructor
     public static class Config {
         private int min;
         private int max;
